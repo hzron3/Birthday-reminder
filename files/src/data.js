47f8d3f0f0be1f71ed9f07ebc89aab157d58ca41
Hzron03 <hzron3@gmail.com>
2023-11-07 08:44:09 +0300

    first commit

---
+++ files/src/data.js
@@ -0,0 +1,73 @@
+export const data = [
+  {
+    id: 1,
+    name: 'Bertie Yates',
+    age: 29,
+    image:
+      'https://res.cloudinary.com/diqqf3eq2/image/upload/v1595959131/person-2_ipcjws.jpg',
+  },
+  {
+    id: 2,
+    name: 'Hester Hogan',
+    age: 32,
+    image:
+      'https://res.cloudinary.com/diqqf3eq2/image/upload/v1595959131/person-3_rxtqvi.jpg',
+  },
+  {
+    id: 3,
+    name: 'Larry Little',
+    age: 36,
+    image:
+      'https://res.cloudinary.com/diqqf3eq2/image/upload/v1586883423/person-4_t9nxjt.jpg',
+  },
+  {
+    id: 4,
+    name: 'Selah Abram',
+    age: 30,
+    image:
+      'https://res.cloudinary.com/diqqf3eq2/image/upload/v1586883417/person-3_ipa0mj.jpg',
+  },
+  {
+    id: 5,
+    name: 'Lola Gardner',
+    age: 29,
+    image:
+      'https://res.cloudinary.com/diqqf3eq2/image/upload/v1586883334/person-1_rfzshl.jpg',
+  },
+    {
+      id: 6,
+      name: 'Yonah Masach',
+      age: 26,
+      image:
+        'https://res.cloudinary.com/diqqf3eq2/image/upload/v1595959131/person-2_ipcjws.jpg',
+    },
+    {
+      id: 7,
+      name: 'Esther Morgan',
+      age: 31,
+      image:
+        'https://res.cloudinary.com/diqqf3eq2/image/upload/v1595959131/person-3_rxtqvi.jpg',
+    },
+    {
+      id: 8,
+      name: 'Lazio Manaucho',
+      age: 39,
+      image:
+        'https://res.cloudinary.com/diqqf3eq2/image/upload/v1586883423/person-4_t9nxjt.jpg',
+    },
+    {
+      id: 9,
+      name: 'Rotich Hezron',
+      age: 23,
+      image:
+        'https://res.cloudinary.com/diqqf3eq2/image/upload/v1586883417/person-3_ipa0mj.jpg',
+    },
+    {
+      id: 10,
+      name: 'Gloria Amadii',
+      age: 31,
+      image:
+        'https://res.cloudinary.com/diqqf3eq2/image/upload/v1586883334/person-1_rfzshl.jpg',
+    }
+  
+];
